23c51096ce3e818fbea810666efd5afacaf0e19d
lisab1993 <brown.lisa999@gmail.com>
2022-09-24 01:52:24 -0700

    finishing touches

--- chirpproject/chirpproject/urls.py
+++ chirpproject/chirpproject/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# Since our homepage is located in the chirpapp, we need make our route to the chirpapp '' so we can load the homepage when the app is launched (in chirpapp.urls.py, you'll also need to point the index route to '' for this to work). Otherwise, we'll get a 'page not found' error that requires us to enter the route by hand. 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
